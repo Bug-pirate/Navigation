bfb8f4b2171ecaa36810e8a12e25c9e60ca8a604
Bug-pirate <patiladitya914@gmail.com>
2025-07-17 11:40:03 +0530

    Formatted

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,13 @@
 import React from "react";
-import Navbar from '../components/Navbar.jsx'
+import Navbar from '../components/Navbar.jsx';
+import './Home.css';
 
 function Home() {
   return (
-    <div
-      className="my"
-      style={{
-        display: "flex",
-        flexDirection:'column',
-        height: "100vh",
-        overflow: "hidden",
-        alignItems: "center",
-      }}
-    >
-      < Navbar />
-      <h1 style={{ color: "black",  fontSize:'80px'
-       }}>Welcome to Navigation</h1>
-      <p style={{paddingLeft:'100px', paddingRight:'100px'}}>
+    <div className="gradient home-page">
+      <Navbar />
+      <h1>Welcome to Navigation</h1>
+      <p>
         we Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum
         dolores illum corporis sequi atque exercitationem quas nobis,
         perferendis nemo rerum ratione, soluta, porro molestias tempora. Eos
