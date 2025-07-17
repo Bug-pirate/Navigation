b4a1d5f64e7cd8580d0acacaa01bf20b1ec06565
Bug-pirate <patiladitya914@gmail.com>
2025-07-17 11:56:48 +0530

    formatted

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from '../components/Navbar.jsx';
 import './Home.css';
 import Map from "../components/Map.jsx";
+import Input from "../components/Input.jsx"
 
 function Home() {
   return (
@@ -11,6 +12,8 @@ function Home() {
       <p>
       Discover the fastest route between any two points with our intelligent pathfinding system. Powered by Dijkstra’s Algorithm, our platform calculates the most efficient and accurate path for your journey in real-time. Whether you're planning a trip or exploring new routes, our system ensures you reach your destination with ease.
       </p>
+      <Input />
+      <Map />
     </div>
   );
 }
