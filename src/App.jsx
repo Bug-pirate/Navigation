3232605bc4f05a5b41cd7242363f089128147be2
Bug-pirate <patiladitya914@gmail.com>
2025-07-17 11:24:06 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 
 import './App.css'
+import Navbar from './components/Navbar.jsx'
 
 function App() {
   
 
   return (
     <>
-      
+      <Navbar />
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import './Navbar.css'
 
 function Navbar() {
   return (
-    <div>
-      
+    <div className='navbar'>
+      <div className='navbar-box'>
+        <div className='nav-link'>
+          <a href="">Home</a>
+        </div>
+        <div className='nav-link'>
+          <a href="">About</a>
+        </div>
+        <div className='nav-link'>
+          <a href="">Contact</a>
+        </div>
+      </div>
     </div>
   )
 }
